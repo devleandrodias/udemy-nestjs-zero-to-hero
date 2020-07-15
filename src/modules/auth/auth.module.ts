e1fefb6131e44cd4580457d139651c7a4d319923
devleandrodias <leandrodbdias@gmail.com>
2020-07-15 15:25:44 -0300

    adicionando bibliotecas para trabalhar com token jwt e passport

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthService } from './services/auth.service';
 import { AuthController } from './controllers/auth.controller';
 import { UserRepository } from './repositories/user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt'
+    }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600
+      }
+    }),
+    TypeOrmModule.forFeature([UserRepository])
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
