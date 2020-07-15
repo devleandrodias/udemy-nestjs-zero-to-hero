6fd93866816c9aab0ec6f0f6f96c78f0317d5cc1
devleandrodias <leandrodbdias@gmail.com>
2020-07-15 09:09:11 -0300

    adicionando configuracoes de banco de dados

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TaskModule } from './tasks/tasks.module';
+import { typeOrmConfig } from 'src/configs/typeorm.config';
 
 @Module({
-  imports: [TaskModule]
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TaskModule]
 })
 export class AppModule {}
