5825b1ac7e3ba60c7696f1c3a216b23cec111af6
devleandrodias <leandrodbdias@gmail.com>
2020-07-15 09:17:27 -0300

    criando camada de respositório com NestJS

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TaskService } from './services/tasks.service';
 import { TaskController } from './controllers/tasks.controller';
+import { TaskRepository } from './repositories/task.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TaskRepository])],
   providers: [TaskService],
   controllers: [TaskController]
 })
