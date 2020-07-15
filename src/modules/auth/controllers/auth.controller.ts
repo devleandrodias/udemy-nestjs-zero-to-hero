3cf3498bc0a495668e1aa8889b633b51e7747965
devleandrodias <leandrodbdias@gmail.com>
2020-07-15 14:05:57 -0300

    adicionando validacoes ao DTO de usuário

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 
 import { AuthService } from '../services/auth.service';
 import { AuthCredentialsDto } from '../dtos/auth-credentials.dto';
@@ -8,7 +8,7 @@ export class AuthController {
   constructor(private readonly _authService: AuthService) {}
 
   @Post('/singup')
-  async singUp(@Body() data: AuthCredentialsDto): Promise<void> {
+  async singUp(@Body(ValidationPipe) data: AuthCredentialsDto): Promise<void> {
     return await this._authService.singUp(data);
   }
 }
