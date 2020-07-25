69479aae20b7e29246029195009fb753e1ac28eb
devleandrodias <leandrodbdias@gmail.com>
2020-07-25 12:50:04 -0300

    instalando mongo db, e typeorm e criando entidade do typeorm

--- graphql-mongodb/src/app.module.ts
+++ graphql-mongodb/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
+import { Lesson } from './lesson/lesson.entity';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
