253c0e87e52639e1a2e1432ad726c4ea796cd647
devleandrodias <leandrodbdias@gmail.com>
2020-07-25 13:19:58 -0300

    criando mutations para inserir dados no banco de dados

--- graphql-mongodb/src/lesson/lesson.resolver.ts
+++ graphql-mongodb/src/lesson/lesson.resolver.ts
@@ -1,8 +1,11 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.type';
+import { LessonService } from './lesson.service';
 
 @Resolver(() => LessonType)
 export class LessonResolver {
+  constructor(private readonly _service: LessonService) {}
+
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   @Query(() => LessonType)
   lesson() {
@@ -12,5 +15,15 @@ export class LessonResolver {
       startDate: new Date().toISOString(),
       endDate: new Date().toISOString(),
     };
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  @Mutation(() => LessonType)
+  createLesson(
+    @Args('name') name: string,
+    @Args('startDate') startDate: string,
+    @Args('endDate') endDate: string,
+  ) {
+    this._service.createLesson(name, startDate, endDate);
   }
 }
